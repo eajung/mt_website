e573defbcf4f033e1d4d53ba1b81371be30a4b92
eajung <eriikjung@gmail.com>
2018-12-20 20:23:28 -0800

    Upadted README and edited profile

--- mt_lab_website/src/components/Header.js
+++ mt_lab_website/src/components/Header.js
@@ -6,8 +6,9 @@ export const Header = () => (
         <div className="content-container">
             <div className="header__content">
                 <Link className="header__title" to="/">
-                    <h1>MT Lab</h1>
-                </Link>
+                    <img src="images/logo.png" alt={"HTML5 Icon"} style={{width: 128, height:128}}/>
+                 </Link>
+                 <h1>MT Lab</h1>
             </div>
         </div>
     </header>
